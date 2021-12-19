37395bf1235e86d218424b64d3da549f745e430f
urybott <95245032+urybott@users.noreply.github.com>
2021-12-19 14:07:48 +0200

    Add files via upload

---
+++ coffee_machine.py
@@ -0,0 +1,16 @@
+
+msg_ = [""] * 20
+msg_[0] = "Starting to make a coffee"
+msg_[1] = 'Grinding coffee beans'
+msg_[2] = 'Boiling water'
+msg_[3] = 'Mixing boiled water with crushed coffee beans'
+msg_[4] = 'Pouring coffee into the cup'
+msg_[5] = 'Pouring some milk into the cup'
+msg_[6] = "Coffee is ready!"
+msg_[7] = ''
+msg_[8] = ""
+msg_[9] = ""
+
+for m in range(7):
+    print(msg_[m])
+    

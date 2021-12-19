3264806b5a8a6a2d41d95ea037b834f6f4317f44
urybott <95245032+urybott@users.noreply.github.com>
2021-12-19 14:23:39 +0200

    Add files via upload

--- coffee_machine.py
+++ coffee_machine.py
@@ -7,10 +7,20 @@ msg_[3] = 'Mixing boiled water with crushed coffee beans'
 msg_[4] = 'Pouring coffee into the cup'
 msg_[5] = 'Pouring some milk into the cup'
 msg_[6] = "Coffee is ready!"
-msg_[7] = ''
-msg_[8] = ""
-msg_[9] = ""
+msg_[7] = 'Write how many cups of coffee you will need:'
+msg_[8] = "For {} cups of coffee you will need:"
+msg_[9] = "ml of water"
+msg_[10] = "ml of milk"
+msg_[11] = "g of coffee beans"
+# msg_[1] = ""
 
-for m in range(7):
-    print(msg_[m])
-    
+one = {'water':200, "milk":50, "beans":15}
+# for m in range(7):
+#     print(msg_[m])
+print(msg_[7])
+cups = int(input())
+print(msg_[8].format(cups))
+print(one['water'] * cups, msg_[9])
+print(one['milk'] * cups, msg_[10])
+print(one['beans'] * cups, msg_[11])
+
